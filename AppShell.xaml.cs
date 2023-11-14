564b4b8ceb74c9ba9609adc0502ff44e6999509d
hardeep <iharry6857@gmail.com>
2023-11-14 16:22:36 +0530

    Added a new folder named Views which Contains 3 Pages inside it.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace Contacts;
+﻿using Contacts.Views;
+
+namespace Contacts;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
-	}
+
+        Routing.RegisterRoute(nameof(ContactPage), typeof(ContactPage));
+        Routing.RegisterRoute(nameof(EditContactPage), typeof(EditContactPage));
+        Routing.RegisterRoute(nameof(AddContactPage), typeof(AddContactPage));
+    }
 }
